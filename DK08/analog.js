cc7c1111f0eac9ead7dec09da704d543b14d915b
yngv27 <61598758+yngv27@users.noreply.github.com>
2021-08-09 09:19:54 -0400

    Update analog.js
    now flips lite/dark bkgd

--- DK08/analog.js
+++ DK08/analog.js
@@ -1,26 +1,40 @@
+let bgc=15,fgc=0;
 var img = {
   width : 176, height : 176, bpp : 2,
   transparent : 0,
-  palette : new Uint16Array([0,8,8,8]),
+  palette : new Uint16Array([7,7,7,fgc]),
   buffer : require("heatshrink").decompress(atob("ABEBoALK3AZLqArQgeQFa0B8ArQgQSKFZkG6CDR8grWheAWCIfKFZkaZBQAHtorWlwqRgEeH5IrMvIrShYrW2grSg+QFakB8ArSgQUJFZcG6ArSgJsJFZcLwArSgF4FakeFScAlorUCpIALj1AFad5FaiZJFZTFKABcGyArSgXgFagWJFZUDIBAAMNxIrKTBIANvArShQqVgEkFaUFFayNJFZIAhFddQFdQA/AH4A/AH4A/AH4A/AH4A/ACMGoAqogkLyArotMG/IqngQpBt5YnrYoBg3pFUsB3zZBgOubssb8gEHAEJSELgIqjgzZBLonQFcVvbIIyKK0YzIADdb0A0HFcO+bIYADgIIHADNkf8YA/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A7gNVAAVAFcsK/4ACyArlg2qAAQrmAH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/AH4A/ABcCyAigsgIHjYrhv1AA4sB3wIGADUb8gHNADcB3RPFt2AacVrU4kG/L/jEotvbMImHgO5bMIADjTVCjfgFUhTB31AAQQrlKgPkgxaCLE25hTZlAAdpgIqogEEFVIA/AH4A/AH4A/AH4A/AH4A/AH4AhlAJIjAJIjIrW3AJIloJIjwqVgO0FaULoArUgXgFaUHyArUCxQrJIBQALheAFaUB8grUjyaJFZLwKABYgKBRVtFahBKFZUeFScB+grUhaZJABLxLFZUHeJIUVFZRCLABEbNhQrKgPkFaUuBZQrKgF4FaVpFawLLAA+4D60eTZSXTFZcLFaMC6ArWg+QFaEDwArWIhI"))
 };
 var isec = {
   width : 3, height : 176, bpp : 2,
   transparent : 0,
-  palette : new Uint16Array([12,12]),
+  palette : new Uint16Array([4,4]),
   buffer : require("heatshrink").decompress(atob("AAMEiFBAX4CDQPQ="))
 };
 var imin = {
+  /*
   width : 5, height : 176, bpp : 2,
   transparent : 0,
   palette : new Uint16Array([15,15,0,0]),
   buffer : require("heatshrink").decompress(atob("AAcQisFoNQqgE/Als3Wn4AiA"))
+  */
+  width : 5, height : 176, bpp : 2,
+  transparent : 0,
+  palette : new Uint16Array([8,fgc,8,8]),
+  buffer : require("heatshrink").decompress(atob("AAMC0VWyultNaAn4Eu6uO4C4/AEA"))
+
 };
 var ihr = {
+  /*
   width : 7, height : 176, bpp : 2,
   transparent : 0,
   palette : new Uint16Array([15,15,0,0]),
   buffer : require("heatshrink").decompress(atob("AHMVgNVitVAH4AmstUhtgV/4A/ABIA=="))
+  */
+  width : 7, height : 176, bpp : 2,
+  transparent : 0,
+  palette : new Uint16Array([8,fgc,8,8]),
+  buffer : require("heatshrink").decompress(atob("AHMqgVWktWytaqtpqulA34Gg3NYh3wV/4A/ABIA="))
 }
 ;
 require("omnigo2.fnt").add(Graphics);
@@ -39,12 +53,12 @@ let drawTime = (d, nm) => {
   let minRot = Math.PI* 2 * d.min / 60 ;
   let secRot = Math.PI* 2 * d.sec / 60 ;
   
-  g.sc(0);
+  g.sc(bgc);
   g.fillCircle(88,118,10);
 
   g.sc(12);
   g.fillCircle(88,88,6);
-  g.sc(15);
+  g.sc(fgc);
   let s = "SUNMONTUEWEDTHUFRISAT".substr(d.dow*3,3) + ' ' + d.dt;
   //console.log(d.dow, s);
   g.drawString(s, 164-g.stringWidth(s), 84);
@@ -56,7 +70,7 @@ let drawTime = (d, nm) => {
   g.drawImage(ihr, 88, 88, { rotate: hrRot } );
   g.drawImage(imin, 88, 88, { rotate: minRot } );
   g.drawImage(isec, 88, 88, { rotate: secRot } );
-  g.sc(0);
+  g.sc(bgc);
   g.fillCircle(88,88,3);
   g.flip();
 };
@@ -66,7 +80,8 @@ eval(require("Storage").read("dk08.js"));
 
 
 function thing() {
-  g.clear();
+  g.sc(bgc);
+  g.fillRect(0,0,175,175);
   g.drawImage(img,0,0);
   //g.flip();
   let dt=new Date();
@@ -75,10 +90,23 @@ function thing() {
 }
 g.setFont("Omnigo",1);
 
-setWatch(()=>{
-  g.bl(0.1);
-  setTimeout(g.bl, 5000, 0);
-}, BTN1, {edge: 'rising', debounce: 100, repeat: true});
+let lpto;
+function swapColor() {
+  let x=fgc;
+  fgc=bgc;
+  bgc=x;
+}
 
+function liteOn() {
+  g.bl(0.05);
+  setTimeout(g.bl, 5000, 0);
+  lpto = setTimeout(swapColor,2000);
+  setWatch(()=>{
+    if(lpto) clearTimeout(lpto);
+    setWatch(liteOn, BTN1, {edge: 'rising', debounce: 100, repeat: false});
+  }, BTN1, {edge: 'falling', debounce: 100, repeat: false});
+}
+
+setWatch(liteOn, BTN1, {edge: 'rising', debounce: 100, repeat: false});
 //setTimeout(thing, 1000);
 setInterval(thing, 1000);
