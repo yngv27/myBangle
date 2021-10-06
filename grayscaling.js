f8c38e7a2a3cd3abc3408babb22d89894a7b6d19
yngv27@gmail.com <Anthro29!>
2021-10-06 17:16:52 -0400

    becoming clock worthy

--- grayscaling.js
+++ grayscaling.js
@@ -1,9 +1,11 @@
-Bangle.setLCDPower(true);
+if(typeof Bangle != 'undefined') { 
+  Bangle.setLCDPower(true);
+} else {
+  //P8
+  P8.wake();
+}
+//g.setBgColor("#ffffff");
 g.clear();
-
-
-let gdata;
-let img;
 
 function pct2col(r,g,b,p) {
   let r1 = Math.floor(r*32*p);
@@ -16,28 +18,28 @@ function pct2col(r,g,b,p) {
   return (r1 << 11) + (g1 << 6) + b1;
 }
 
-let r = 0.83, gr = 0.2, b = 0.66;
+let red = [0.99, 0.83, 0.2, 0.8];
+let green = [0, 0.8, 0.99, 0.2];
+let blue = [0.2, 0.2, 0.99, 0.99];
+let coords = [ {x:20, y:0}, {x:120, y:0}, {x:20, y:100}, {x:120,y:100}];
 
-img = {
-  width : 32, height : 32, bpp : 2,
-  transparent : -1,
-  palette : new Uint16Array([0,pct2col(r,gr,b,1),pct2col(r,gr,b,0.625), pct2col(r,gr,b,0.325)]),
-  _palette : new Uint16Array([65535,32,23275,40179]),
-  buffer : E.toArrayBuffer(atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFVVVQAOVsAAVVVVACVVXABVVVUAlVVUAFVVVQBVaVYAVaqqAlXCVQBVAAABVQNrA1UpsA1WDwADVVVYDVaVbANVVVYNVVVXA1VVVYlVVVUDpcJViVW5VcAAA1VNVQFVg+sAVU1WA1WBVQJVgVYCVcJVVVWDVblVA1VVVgBVVVcA1VVYACVVWAAJVXAADVWwAAAwAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="))
+eval(require("Storage").read("eurostile.fnt"));
+let dt = new Date();
+let h1 = dt.getHours();
+let h2 = h1 % 10;
+h1 = Math.floor(h1/10);
+let m1 = dt.getMinutes();
+let m2 = m1 % 10;
+m1 = Math.floor(m1/10);
+let imgs = [eurofont[h1], eurofont[h2], eurofont[m1], eurofont[m2]];
 
-};
+for(let idx=0; idx<4; idx++) {
+  let r1=red[idx];
+  let g1=green[idx];
+  let b1=blue[idx];
+  imgs[idx].palette =  new Uint16Array([pct2col(r1,g1,b1,0),pct2col(r1,g1,b1,1), pct2col(r1,g1,b1,0.5), pct2col(r1,g1,b1,0.75)]);
+  //g.drawImage(img, 60*idx, 40);
+  g.drawImage(imgs[idx],coords[idx].x,coords[idx].y, {scale: 2.75});
 
-gdata = img.buffer;
-
-g.drawImage(img, 0, 40);
-g.drawImage(img, 0, 120, {scale: 4});
-
-let output='';
-for(let idx=0; idx < gdata.length; idx++) {
-  if(idx % 8 == 0) {
-    console.log(output);
-    output = '';
-  }
-  output += `0x${gdata[idx].toString(2)} `;
 }
-console.log(output);
+
