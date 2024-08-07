6449dcf87ccc9595e3316360bb78f2ff00fd2a43
yngv27@gmail.com <Anthro29!>
2024-08-06 20:15:50 -0400

    debugging

--- garage/gdRx.js
+++ garage/gdRx.js
@@ -1,3 +1,5 @@
+
+
 // we are GD module!
 let opts = {
   cs: D29, 
@@ -39,32 +41,65 @@ let mastKey1 = E.toUint8Array(atob("7XwmpfDcSf8onWkLaAK10g=="));
 let mastKey2 = E.toUint8Array(atob("CXDpA6GJFFZJqQzoGxl83A=="));
 let tempKey = "";
 
+function flush() {
+  // set to IDLE first
+  r.cmd(0x36); r.delay(20);
+  // flush buffers
+  r.cmd(0x3b); r.delay(20);
+  r.cmd(0x3a); r.delay(20);
+  setTimeout(()=>{
+    //r.init();
+    r.listen();
+  }, 500);
+}
+
+let maxErr = 10;
+let ival=0;
 function challenge() {
   let buf = r.recv();
   debug(`GOT: `+buf);
-  debug("That's "+buf.length);
+  if(ival) clearInterval(ival);
+  //debug("That's "+buf.length);
   let msg = E.toString(buf.slice(4,61)).trim();
-  debug(`GOT [${msg}] pf ${msg.length}`);
+  debug(`GOT [${msg}] len: ${msg.length}`);
   //*
   if(msg == "STAGE1") { 
     tempKey = getRandKey();
     debug("TK: "+btoa(tempKey));
-    debug("send: "+ btoa(encode(tempKey, mastKey1))); 
-    r.send(btoa(encode(tempKey, mastKey1))); 
-  }
-  else {
+    setTimeout((val)=>{
+      debug("send: "+ btoa(encode(tempKey, mastKey1))); 
+      r.send(btoa(val));
+    }, 50, encode(tempKey, mastKey1)); 
+//      r.recv();
+    r.listen();
+  } else if(msg.length == 24 && buf[1] == 59) {
     debug("RECD: "+msg);
     resp = decode(E.toUint8Array(atob(msg)), mastKey2);
     debug("decoded: "+btoa(resp));
     if(resp == tempKey) {
+      //setTimeout(()=>{
+        r.send("STAGE4"); //ack
+      //}, 50);
       D13.set();
       D24.reset();
-      debug("CLICK");
-      setTimeout(E.reboot, 500);
+      debug("CLICK\n");
+      //setTimeout(E.reboot, 500);
+      setTimeout(()=>{
+        D13.reset();
+        D24.set();
+        //flush();
+      }, 500);
     } else {
+      r.recv();
       r.listen();
     }
+  } else {
+    if(!(maxErr--)) E.reboot(); //panic 
+    r.recv();
+    r.listen();
   }
+  //ival=setInterval(()=>{
+  //flush();
   //*/
   
 }
@@ -75,9 +110,7 @@ function nextResponse(str) {
 
 
 let gd0Int = 0;
-/*function setInt() { if(!gd0Int) gd0Int = setWatch(challenge, opts.gd0, {edge: "rising", repeat: true});}
-function clearInt() { if(gd0Int) {clearWatch(gd0Int); gd0Int=0;}}
-*/
+//function setInt() { if(!gd0Int) gd0Int = setWatch(challenge, opts.gd0, {edge: "rising", repeat: true});}
 function setInt() {
   gd0Int = setInterval(()=>{
     if(r.readReg(0xfb) > 0) challenge();
@@ -85,9 +118,10 @@ function setInt() {
 }
 //function clearInt() { if(gd0Int) {clearWatch(gd0Int); gd0Int=0;}}
 function clearInt() { if(gd0Int) {clearInterval(gd0Int); gd0Int=0;}}
+
 setInt();
 r.listen();
 
-setInterval(()=>{r.recv();r.listen();}, 2500);
+//ival = setInterval(()=>{r.recv();r.listen();}, 2500);
 
 
