3ee8510107f8cd22e843c0e50aad38f71488c16e
yngv27@gmail.com <Anthro29!>
2024-12-09 13:42:59 -0500

    making st7789 a proper module

---
+++ APW/240x240/gshock2.js
@@ -0,0 +1,223 @@
+Modules.addCached("FontDylex7x13",function(){var a=atob("AAAAAAAAA/QAAcAAAHAAAAJAf4CQH+AkAAAMQJIP+CSBGAAAQAUIEYAwBiBCgAgAAG4EiCRA0gBgDIAAOAAAAfAwYgCAAIAjBgfAAACgAgB8AIAKAAABAAgB8AIAEAAAACAOAAAIAEACABAAgAAADAAAAYAwBgDAGAAAAfgQIJkECB+AAAIQIIP8ACABAAAQwQoIkEiBhAAAQgQIJEEiBuAAADACgCQCID/ACAAAeQJEEiCRBHAAAHwFEEiCRAHAAAQAIMEYCwBgAAANwJEEiCRA3AAAOAIkESCKA+AAAGYAAAAgzgAACACgCICCAAAKAFACgBQAoAAAggIgCgAgAABABAAjQSAGAAAA8AhAmQUoL0CKA4AAABwHAMgGQA4ADgAAf4JEEiCRA4gDgAADwCECBBAggQIQAAH+CBBAggQIQDwAAD/BIgkQSIJEECAAB/gkASAJAEAAAAeAQgQIIkESBOAAA/wCABAAgAQB/gAAQIP8ECAAABAAQQIIEH8AAB/gEADACQCECBAAA/wAIAEACABAAA/wMABgAwBgB/gAAf4MABgAMABg/wAADwCECBBAgQgHgAAH+CIBEAiAOAAAB4BCBAggQIQD2AAD/BEAiARgHIACAAAxAkQSIIkESBGAAAgAQAIAH+CABAAgAAAP4ACABAAgAQfwAAHAAcABgAwDgOAAAD4ADgGAMABgAOD4AAAwwEgBgAwAkBhgAAYACAAgAPAIAIAYAAAEGCFBEgkQUIMEAAH/yAJAEAAYADAAYADAAYAAQBIAn/wAAGAMAYADAAYAAAAIAEACABAAgAQAIAAQAEAAAADAKQFICkA+AAD/gIQEICEA8AAAPAIQEICEAkAAAPAIQEICEP+AAAPAKQFICkA0AAAQA/wkASAIAAAAPAISEJCEh/gAD/gIAEACAA+AAAQBPwAAABAAggSfwAA/4AQAYASAQgAAgAf8AAA/AQAIAD4CABAAfAAAPwEACABAAfAAAHgEICEBCAeAAAP+EICEBCAeAAAHgEICEBCA/4AAPwCACABAAQAAAEQFICkBKAiAAAIAfwCEBCABAAAPgAIAEACA/AAAMABgAMAYAwAAAPAAYAYAwAGABgPAAACEAkAMAJAIQAAD5ACQBIAkP8AACEBGAlAUgMQAAAgAQD3iAJAEAAf/AAEASAI94BAAgAAAIAIAEADAAgAQAQAAAFAHwFUCqBBARAAAACAOAAAAQQI/4kASAAAADgAAA4AAAEAAABAAAAQAAEACAH/AgAQAAAFACgH/AoAUAAAEAEAEABAAQAAAGMAYAwBjAAAAwAADEKRDIiiQRIEYAAAIAKAIgAAH4ECCBA/AkQSIIEAACDFChiRSIKEGCAADAAQAAAEAMAAADAAQAwAEAAABADAAQAwAAAAQAcAfAHABAAAAQAIAEACABAAAAQAIAEACABAAgAQAAAgAgAIAIAAACAB4AgAAAPAGADwAAAEQlIKkJKAiAAAIgCgAgAAAeAQgIQDwCkBSAaAAAIQkYKUJSAxAAAYACAQgAOEIAIAYAAAL8AAAeAQgf4EIBIAAATA+gkQSIAEAABBAfAIgEQCIB8BBAAAwAEgBQAeAUASAwAAAffAADCCYhKQjIIYAAEAAABAAAAH4ECCZBSgpQQIH4AAAQBUAqAPAAAAQAUAVAFAEQAAAQAIAEADwAAH4ECC9BUglQQIH4AAIAEACABAAgAQAIAAAAwAkASAGAAAAIgEQPoBEAiAACIBMAqAJAAAEQCoBUAUAAAEAEAAAAAEH8AIACABAfAAQAAGAHgD/hAA/4QAAAA4AcAOAAAAFADAACQD4AEAAAOAIgEQBwAAAEQBQBUAUAEAAA8YAwBkDGGHgAgAAeMAYAwBpjFQBIAAIgFTB2AMgYww8AEAAADACQWIAEAEAAADgOBJAUgBwAHAAABwHAUgSQA4ADgAAA4TgSQJICcABwAAAcJwJICkCOAA4AAAOE4AkASAnAAcAAAHDcCSBJAbgAOAAADgGANAIgH+CRBAgAAHgEIECSBxAgQgAAH8SSFJAkgQQAAH8CSFJEkgQQAAH8KSJJCkgQQAAH8KSBJCkgQQAAEET+FBAAAQQv4kEAAFBE/hQQAAUED+FBAAACAP4EkCSBBARAHAAAH8KAIwCGCAwP4AAA4AiEghQQEQBwAAAcARBQRIICIA4AAAOBIhIIkEJEAcAAAHAkQkEKCIiAOAAADgSICCBBCRAHAAACIAoAIAKAIgAAD0CECNBYgQgXgAAD8ABEAhAQAIH4AAB+AAhARAIAED8AAA/BARAIgEICB+AAAfggIAEACEBA/AAAMABAAQEHEEAEAMAAAH+AkASAJADAAAABD/CQhIQkINEAcAAADAKQlIKkA+AAADAKQVISkA+AAADAqQlIKkA+AAADAqQlIKkI+AAADAqQFIKkA+AAADBKRVISkA+AAADAKQFIB8BSApANAAADwCEhDghAJAAADwSkFSApANAAADwKkJSApANAAADwKkJSCpANAAADwKkBSCpANAAAkAL8AACgCfgAAUAT8EAAACQAPwgAAAAcERCogkQvwAAF+EgBQBIAD4AAA8EhBQgIQDwAAA8AhBQhIQDwAAA8ChCQgoQDwAAA8ChCQgoQjwAAA8ChAQgoQDwAAAQAIAVACABAAAA9AjAWgMQLwAAB8EBBAgAQH4AAB8CBCAgAQH4AAB8CBCAggQH4AAB8CBAAggQH4AAB8ABJAlASH+AAH/ghAQgIQDwAAB8CBIAkgSH+AAA"),
+b=atob("AwIEBgYIBwIEBAYGAwYCBgYGBgYHBgYGBgYCAwUGBQYIBwcHBwcGBwcEBgcGBwcHBgcHBwgHBwgHCAcEBgQGCAMGBgYGBgYGBgMFBgMIBgYGBgYGBgYGCAYGBgYCBggABwADBgQGBgYGBwcECAAHAAADAwUFBgYIBQgGBAgABggAAgYGCAgCBgQIBQYFAAgIBQYFBQMIBwQDBAUGBwcIBgcHBwcHBwgHBgYGBgQEBAQIBwcHBwcHBgcHBwcHCAYIBgYGBgYGCAYGBgYGAwMEBAYGBgYGBgYGBgYGBgYGBgY=");exports.add=function(c){c.prototype.setFontDylex7x13=function(){this.setFontCustom(a,32,b,13)}}});
+
+function getDigit(d) {
+  return(_S.read("gshock36.fnt",
+  [0, 432, 864, 1296, 1728, 2160, 2592, 3024, 3456, 3888 ][d],  
+  [432, 432, 432, 432, 432, 432, 432, 432, 432, 432, ][d]
+  ));
+}
+function getDigitMini(d) {
+  return(_S.read("gshock30.fnt",
+  [0, 321, 642, 963, 1284, 1605, 1926, 2247, 2568, 2889][d],
+  [321, 321, 321, 321, 321, 321, 321, 321, 321, 321][d]
+ ));
+}
+
+/* Fun Function
+function str2ab(str) {
+    let buf = new Uint16Array(str.length/2);
+    for (let i=0; i < str.length; i++) {
+      buf[i] = str.charCodeAt(i*2)+(str.charCodeAt(i*2+1)<<8);
+    }
+    return buf;
+}
+function getDigitObj(img) {
+  return {
+    width: img.charCodeAt(0),
+    height: img.charCodeAt(1),
+    bpp: img.charCodeAt(2) & 63, //remove transparent bit.    
+    transparent: 0,
+    palette: str2ab(img.substring(4,36)),
+    buffer: img.substring(36),
+  };
+}
+function palAdjust(pal, pct) {
+  // pal is Uint16Array of 565 RGB
+  for(let c=0; c<16; c++) {
+    let r1=pal[c] >> 11, g1=(pal[c] >> 5) & 63, b1=pal[c] & 31;
+    r1 = Math.floor(r1*pct[0]); g1 = Math.floor(g1*pct[1]); b1 = Math.floor(b1*pct[2]); 
+    pal[c] = (((r1<<6)+g1)<<5)+b1;
+  }
+  return pal;
+}
+
+*/
+
+exports={};
+
+const C ={
+  BGCOLOR: "#181838", //"#2F3080";.
+  FGCOLOR: "#e0e0d0", //"#e0d080";
+};
+
+function drawDigit(d, x, y) {
+  g.drawImage(getDigit(d), x, y, {scale: 1.5});
+}
+
+function drawDigitMini(d,x, y) {
+  g.drawImage(getDigitMini(d), x, y);
+}
+
+let lastTime = '';
+let lastDate = '';
+let lastSecs = '';
+
+//g.setBgColor(BGCOLOR).setColor(FGCOLOR).fillCircle(78,112,2).fillCircle(78,140,2);
+require("FontDylex7x13").add(Graphics);
+g.setFont("Dylex7x13",1).setFontAlign(0,-1);
+
+let tikint = 0;
+let lastDigs = new Uint8Array([11,11,11,11]);
+
+exports.start = function () {
+  g.setBgColor(C.BGCOLOR).clear();
+  wOS.on("tick", tick);
+};
+
+let drawClock = function(dt) {
+  let digs = new Uint8Array([Math.floor(dt.hr / 10), dt.hr % 10, Math.floor(dt.min / 10), dt.min % 10]);
+  let x=18;
+  for(let d = 0; d < digs.length; d++) {
+    if(digs[d] == lastDigs[d]) continue;
+    //if( !d && !digs[d]) continue;
+    // only when changing hour
+    if(d == 1) drawAmPm(dt.isPM);
+    if(d < 2) g.setColor("#80E0E0"); else g.setColor(C.FGCOLOR);
+    // drawDigit(digs[d], [50,72,101,123][d], 48); //unscaled
+    drawDigit(digs[d], [x,x+32,x+76,x+106][d], 44); //unscaled
+  }
+  lastDigs = digs;
+  tick();
+};
+exports.drawClock = drawClock;
+
+function tick() {
+  let s = new Date().getSeconds();
+  //if(!s) { drawClock(); return; }
+  let x=166;
+
+  let digs = [Math.floor(s / 10), s % 10];
+  let ldigs = [Math.floor(lastSecs / 10), lastSecs % 10];
+  for(let d = 0; d < digs.length; d++) {
+    if(digs[d] != ldigs[d])
+      drawDigitMini(digs[d], [x,x+17][d], 67);
+  }
+  lastSecs = s;
+}
+logD=print;
+exports.drawData = function (dt) {
+  //let dtstr = dt.toString().substring(0,16);
+  dt=new Date();
+  let m = dt.getMonth() +1;
+  let d = dt.getDate();
+  let x = 116;
+  //logD("in: lastDate="+lastDate);
+  if (lastDate != d) {
+    logD("Success");
+    lastDate = d;
+    let digs = [Math.floor(m / 10), m % 10, Math.floor(d / 10), d % 10];
+    for(let d = 0; d < digs.length; d++) {
+      //if(!d && !digs[d]) continue;
+      logD("loop");
+      drawDigitMini(digs[d], [x,x+19,x+50,x+69][d], 108);
+    }
+    drawDOW(dt.getDay());
+  }
+  //logD("out");
+  //drawBattery(E.getBattery());
+  let b = E.getBattery();
+  g.drawString(` ${b} % `, x+100, 20, true);
+  g.drawString(` ${b} % `, x+101, 20,false);
+  
+  if(typeof(future) != "undefined")
+    g.drawString(` ${tDiff(future)} `, 120, 154, true);
+};
+
+function drawBattery(b) {
+  let r = 15, steps = 12, lit=steps*b/100 ;
+  for(let i=0; i < steps; i++) {
+    let ang = i * 2 * Math.PI / steps;
+    let x = 161 + Math.sin(ang) * r;
+    let y = 74 - Math.cos(ang) * r;
+    g.setColor(C.BGCOLOR);
+    g.fillCircle(x, y, 1).setColor(0.4,0.7,0.7);
+    if(i > lit) {
+      g.drawCircle(x, y, 1);
+    } else {
+      g.fillCircle(x, y, 1);
+    }
+  }
+}
+
+function drawAmPm(isPM) {
+  g.setColor(C.FGCOLOR);
+  let v = isPM ? "    \nPM " : "AM\n     ";
+  let x = 216;
+  g.clearRect(x, 59, 239, 99);
+  g.drawString(v, x, 59, true);
+  g.drawString(v, x+1, 59, false);
+}
+function drawDOW(dow) {
+  let v = "SUNMONTUEWEDTHUFRISAT".substring(dow*3,(dow+1)*3);
+  let x=216;
+  g.clearRect(208, 124, 239, 144);
+  g.setColor(C.FGCOLOR).drawString(v, x, 126, true);
+  g.setColor(C.FGCOLOR).drawString(v, x+1, 126, false);
+}
+
+function tDiff(moment) {
+  let dt = new Date();
+  let s = 1;
+  let m = Math.floor((dt.getTime()-moment)/60000);
+  if(m < 0) { s=0; m*=-1;}
+  let h = Math.floor(m / 60); m = m % 60;
+  let dz = Math.floor(h / 24); h = h % 24;
+  return(`${s?'+':'-'}${dz}d ${h}h ${m}m`); 
+}
+exports.showMsg = function(msgobj) {
+  g.clearRect(0,180,239,230).setColor(1,1,0.85);
+//  msgo.text.split('|').forEach((s,i)=>{g.drawString(s,120, 184+i*18);});
+  g.setFontAlign(0,-1);
+  g.drawString(g.wrapString(msgobj.text, 160).join("\n"),120, 184);
+
+  //(m, 120, 186);
+  //lastDate = '';
+  //[400,800,1200,2000,2400].forEach((t) => {setTimeout(Bangle.buzz, t, 175);});
+};
+
+exports.stop = function() {
+  wOS.UI.removeAllListeners("tick");
+};
+//*
+
+if(process.env.BOARD == "EMSCRIPTEN") {
+  wOS = { getStepCount: ()=>{return(3456);} };
+  _S = require("Storage");
+} else {
+  wOS.wake();
+}
+exports.start();
+//*
+let dt = { hr: 4, min: 1, niceDate: "Sun Jul 12", isPM: true };
+exports.drawClock(dt);
+exports.drawData(dt);
+exports.showMsg({title:"Title", text:"This would be message 1"});
+//*/
+
+if(typeof(TC) != "undefined") {
+  TC.on("touch", (p) => {
+    if(p.x > 80 && p.x < 160 && p.y > 140) {
+      showBits();
+      //setTimeout(start, 60*1000);
+    }
+  });
+    // swipe dn (1) use it to reset/reprint date
+  TC.on("swipeALL", (d) => {
+    if(d == 1) lastDate = '';
+    drawDate(new Date());
+    lastDigs = new Uint8Array([11,11,11,11]);
+    lastTime = '';
+  });
+
+}
